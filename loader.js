351e6c5c534849c0c68baebb40d6b8f50c1f575c
Ting Xia <ting.xia24@gmail.com>
2016-04-08 15:06:14 -0400

    Added loader

---
+++ loader.js
@@ -0,0 +1,19 @@
+//// loader stuff
+var anyDB = require('any-db');
+var conn = anyDB.createConnection('sqlite3://test.db');
+//create table messages
+conn.query("DROP table IF EXISTS calendar");
+var sqlCreate = "CREATE TABLE calendar (" +
+    "id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+    "start_date TEXT, " +
+    "end_date TEXT, " +
+    "time TEXT, " +
+    "event_name TEXT, " +
+    "event_type TEXT, " +
+    "location TEXT, " +
+    "department TEXT," +
+    "website_link TEXT);";
+conn.query(sqlCreate, function(){
+    console.dir("Made table calendar!");
+});
+
