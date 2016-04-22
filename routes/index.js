a11310ecaaf654c84ea0e8a2f143232ec206d2c0
quanzitao <quanzitao@gmail.com>
2016-04-21 22:11:13 -0400

    Included redirection on upload success/failure.

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,15 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/upload-success', function(reg, res, next) {
+    //console.log("upload-success");
+    //res.send("upload-success");
+    res.render('upload-success', { title: 'The upload was successful!' });
+});
+
+router.get('/upload-failure', function(reg, res, next) {
+    //res.send("upload-failure");
+    res.render('upload-success', { title: 'The upload failed.' });
+});
+
 module.exports = router;
