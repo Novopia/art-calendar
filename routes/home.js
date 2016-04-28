786c25fe1de2027c1a6d5ddace53a1c3960fdc76
Novopia <tomyao0@gmail.com>
2016-04-28 15:46:58 -0400

    Now we can start from the root folder: node bin/www

--- routes/home.js
+++ routes/home.js
@@ -4,7 +4,7 @@
 var express = require('express');
 var router = express.Router();
 var anyDB = require('any-db');
-var conn = anyDB.createConnection('sqlite3://../data/test.db');
+var conn = anyDB.createConnection('sqlite3://data/test.db');
 /* GET home page. */
 router.get('/', function(req, res, next) {
     res.render('front_end/home');
--- routes/upload.js
+++ routes/upload.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var anyDB = require('any-db');
-var conn = anyDB.createConnection('sqlite3://../data/test.db');
+var conn = anyDB.createConnection('sqlite3://data/test.db');
 
 /* GET home page. */
 router.get('/', isLoggedInMiddleware,function(req, res, next) {
