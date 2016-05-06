f1799c2abe36e1cd586f058bd07258bcf82423ed
Ting Xia <ting.xia24@gmail.com>
2016-05-06 13:51:38 -0400

    minor bug fixes

--- public/javascripts/upload/upload.js
+++ public/javascripts/upload/upload.js
@@ -1,21 +1,20 @@
-$.post("/upload/checkLogin", function(responseJSON){
-    console.log("got back from check login");
-    if (responseJSON == "false") {
-        window.location.href = "/login/upload";
-    }
+window.addEventListener('load', function(){
+    $.post("/upload/checkLogin", function(responseJSON) {
+        console.log("got back from check login");
+        if (responseJSON == "false") {
+            window.location.href = "/login/upload";
+        }
+    });
+    // time out after 5 minutes=300 seconds=300000
+    console.log("in side upload. finished load!");
 
-    window.addEventListener('load', function(){
-        // time out after 5 minutes=300 seconds=300000
-        console.log("in side upload. finished load!");
-
-        setTimeout(function(){
-            $.post("/upload/logout", function(responseJSON){
-                console.log("get back");
-                window.location.href = "/login/upload"
-            });
-        }, 300000);
-    }, false);
-});
+    setTimeout(function(){
+        $.post("/upload/logout", function(responseJSON){
+            console.log("get back");
+            window.location.href = "/login/upload"
+        });
+    }, 300000);
+}, false);
 
 
 $(function() {
